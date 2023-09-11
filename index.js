fd7cd2e509c95d1f8b6a8c44b37b719b899b548e
juuju12 <96474358+juuju12@users.noreply.github.com>
2023-09-11 17:10:30 +0700

    index.js

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function viewMenu() { //fungsi untuk menampilkan halaman menu
     console.log("1.Tambah Data \n");
     console.log("2.Lihat Data \n");
     console.log("3.Hapus Data \n");
+    console.log("4.Pencarian Data \n");
+    console.log("5.Hapus data \n");
+    console.log("99.Exit\n");
     readline.question(`Silahkan Masukan Pilihan Anda  :`, input => {
         mainMenu(Number(input))
     });
@@ -34,6 +37,19 @@ function mainMenu(pilihan) { // fungsi untuk mengatur pilihan menu
             lihatData() 
             break;
         // lanjutkan menu pilihanya disini secara urut
+        case 3:
+            resetData()
+            break;
+        case 4:
+            pencarianData()
+            break;
+        case 5:
+            hapusData()
+            break;
+        case 99:
+            console.log("----SELESAI-----");
+            readline.close();
+            break;
         default:
             console.log("Pilihan Tidak Valid !");
             readline.close()
@@ -46,17 +62,34 @@ function mainMenu(pilihan) { // fungsi untuk mengatur pilihan menu
 function simpan() { // fungsi untuk menyimpan data
     console.log("Silahkan Masukan Data ! : ");
     readline.question("Nama :", (nama) => {
-        objectKontak.nama = nama
-        console.log(`Input data berhasil ! :${nama}`);
-        ambilInputanNomor()
-    })
-    
+        const regex = /^[a-zA-Z]+$/;
+        if (regex.test(nama)) {
+            objectKontak.nama = nama
+            console.log(`Input data berhasil ! :${nama}`);
+            ambilInputanNomor()
+          } else {
+            console.log("Error!! Input hanya berupa huruf, silahkan kembali masukan nama.");
+            simpan()
+            
+          }
+    }) 
 }
 const ambilInputanNomor = () => { // fungsi untuk mengambil inputan nomor
     readline.question("Nomor :", (nomor) => {
-        objectKontak.nomorHp = nomor
-        databaseKontak.push(Object.assign({},objectKontak)) // insert data kedalam array databseKOntak
-        kembali()
+        const regex= /^[0-9]+$/;
+        if(nomor==objectKontak.nomorHp){
+            console.log("Error!! Nomor hp sudah terdaftar.")
+            ambilInputanNomor()
+        }else if (regex.test(nomor)) {
+            objectKontak.nomorHp = nomor
+            databaseKontak.push(Object.assign({},objectKontak)) // insert data kedalam array databseKOntak
+            console.log("Data berhasil disimpan.")
+            kembali()
+          } else {
+            console.log("Error!! Input bukan number, silahkan kembali masukan nomor hp");
+            ambilInputanNomor()
+          }
+        
     })
 }
 const kembali = () => { // fungsi untuk navigasi kembali
@@ -77,13 +110,54 @@ function lihatData () { // fungsi untuk melihat list data
 
 function resetData () {
     // tambahkan fungsi reset  data disini
+    databaseKontak.length = 0;
+    kembali()
 }
 
 function pencarianData () {
     // tambahkan fungsi pencarian data disini 
+    readline.question("Masukkan kata kunci nama: ", (kataKunci) => {
+        const hasilPencarian = cariNama(kataKunci);
+        if (hasilPencarian.length > 0) {
+            console.log(`Hasil pencarian untuk "${kataKunci}":`);
+            hasilPencarian.forEach((hasilNama) => {
+                console.table(hasilNama);
+            });
+        } else {
+            console.log(`Tidak ada hasil untuk "${kataKunci}".`);
+        }
+        kembali();
+    });
+    
+}
+function cariNama(keyword) {
+    const hasilPencarian = databaseKontak.filter((kontak) =>
+        kontak.nama.toLowerCase().includes(keyword.toLowerCase())
+    );
+
+    return hasilPencarian;
 }
 function hapusData () {
     // tambahkan fungsi hapus data data disini 
+    function hapusData() {
+    console.log("Data yang tersedia:");
+    console.table(databaseKontak);
+    readline.question("Masukkan indeks data yang ingin dihapus: ", (indeks) => {
+        indeks = parseInt(indeks);
+
+        if (isNaN(indeks) || indeks < 0 || indeks >= databaseKontak.length) {
+            console.log("Indeks tidak valid.");
+            hapusData();
+            return;
+        }
+
+        const namaYangDihapus = databaseKontak[indeks].nama;
+        databaseKontak.splice(indeks, 1); // Hapus data berdasarkan indeks
+
+        console.log(`Data ${namaYangDihapus} telah dihapus.`);
+        kembali();
+    });
+    
 }
 
 
